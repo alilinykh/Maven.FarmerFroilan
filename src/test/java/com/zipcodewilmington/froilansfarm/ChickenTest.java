377f8323a27c62dd84424407a5495fea3db0f46e
alilinykh <aililnykh@dtcc.edu>
2019-06-21 14:58:47 -0400

    create chx and horse test

--- src/test/java/com/zipcodewilmington/froilansfarm/ChickenTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/ChickenTest.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.zipcodewilmington.froilansfarm.Baskets.CornBasket;
+import com.zipcodewilmington.froilansfarm.Baskets.EggBasket;
+import com.zipcodewilmington.froilansfarm.Edibles.EarCorn;
+import com.zipcodewilmington.froilansfarm.Edibles.Egg;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,10 +12,18 @@ import static org.junit.Assert.*;
 
 public class ChickenTest {
     Chicken chicken;
+    CornBasket cornBasket;
+    EarCorn earCorn;
+    EggBasket eggBasket;
+
 
     @Before
     public void setUp() throws Exception {
         chicken = new Chicken();
+        cornBasket = new CornBasket();
+        earCorn = new EarCorn();
+        eggBasket = new EggBasket();
+
     }
 
     @Test
@@ -20,12 +32,40 @@ public class ChickenTest {
         String actual = chicken.makeNoise();
         Assert.assertEquals(expected,actual);
     }
+    @Test
+    public void makeNoise1() {
+        String expected = "wrongNoise";
+        String actual = chicken.makeNoise();
+        Assert.assertFalse(expected.equals(actual));
+    }
 
     @Test
     public void yield() {
+//        Integer expected = eggBasket.size() + 1;
+//        chicken.yield();
+//        Integer actual = eggBasket.size();
+//
+//        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void eat() {
+        // see how much corn in the basket
+        //Integer expected = cornBasket.size() - 1;
+        //chicken.eat(cornBasket.get(earCorn));
+        //Integer actual = cornBasket.size();
+
+        //Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInheritance() {
+        Assert.assertTrue(chicken instanceof Animal);
+    }
+
+    @Test
+    public void testImplementation() {
+        Assert.assertTrue(chicken instanceof Produce);
+
     }
 }
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Edibles/EarCornTest.java
@@ -0,0 +1,6 @@
+package com.zipcodewilmington.froilansfarm.Edibles;
+
+
+public class EarCornTest {
+
+}
