81926b9ca8836fe7d445f6937821e036b45f00a7
alilinykh <aililnykh@dtcc.edu>
2019-06-23 11:24:20 -0400

    created PotatoPlant test

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/FieldRelated/PotatoPlantTest.java
@@ -0,0 +1,41 @@
+package com.zipcodewilmington.froilansfarm.FieldRelated;
+
+import com.zipcodewilmington.froilansfarm.Baskets.Basket;
+import com.zipcodewilmington.froilansfarm.Edibles.Potato;
+import com.zipcodewilmington.froilansfarm.Edibles.Tomato;
+import com.zipcodewilmington.froilansfarm.Produce;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PotatoPlantTest {
+
+    Basket potatoBasket;
+    PotatoPlant potatoPlant;
+    @Before
+    public void setUp() throws Exception {
+        potatoBasket = new Basket<Potato>();
+        potatoPlant = new PotatoPlant();
+    }
+
+    @Test
+    public void yieldTest() {
+        Integer expected = 1;
+        potatoBasket.add(potatoPlant.yield());
+        Integer actual = potatoBasket.size();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void InheritanceTest() {
+        Assert.assertTrue(potatoPlant instanceof Crop);
+    }
+
+    @Test
+    public void ImplementantationTest() {
+        Assert.assertTrue(potatoPlant instanceof Produce);
+    }
+}
