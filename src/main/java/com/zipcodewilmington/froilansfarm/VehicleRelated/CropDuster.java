0358793f40894f497b35f45a260ac8a3f202abf1
Edward Korkollie <korkollieed@gmail.com>
2019-06-23 12:29:42 -0400

    update

--- src/main/java/com/zipcodewilmington/froilansfarm/VehicleRelated/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/VehicleRelated/CropDuster.java
@@ -1,16 +1,52 @@
 package com.zipcodewilmington.froilansfarm.VehicleRelated;
 
+import com.zipcodewilmington.froilansfarm.FieldRelated.Crop;
 import com.zipcodewilmington.froilansfarm.FieldRelated.CropRow;
+import com.zipcodewilmington.froilansfarm.Person;
+import com.zipcodewilmington.froilansfarm.PersonRelated.Froilanda;
+import com.zipcodewilmington.froilansfarm.PersonRelated.Pilot;
 
-public class CropDuster extends Vehicle implements Aircraft {
+import java.util.ArrayList;
+
+import static com.zipcodewilmington.froilansfarm.PersonRelated.Froilanda.*;
+
+public class CropDuster extends Vehicle implements Aircraft, FarmVehicle {
+
+    private Person pilot;
+    private boolean isFlying;
+    String person = "";
+    private Vehicle vehicle;
+    FarmVehicle farmVehicle = new CropDuster();
+    Aircraft aircraft = new CropDuster();
+
+
     public void fly() {
+        if(vehicle == farmVehicle && vehicle == aircraft) {
+            isFlying = true;
+        }
+        System.out.println("Froilanda is flying");
     }
+
     public void fertilize(CropRow cropRow) {
+
 
     }
 
     public String makeNoise() {
-        return null;
+
+        return "CropDuster noise";
 
     }
+
+    public boolean isFlying() {
+
+        return isFlying;
+    }
+
+    public void harvest(Crop crop) {
+
+
+    }
+
+
 }
