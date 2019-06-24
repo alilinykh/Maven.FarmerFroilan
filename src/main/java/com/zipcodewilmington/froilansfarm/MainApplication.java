901319bb2707e106da80de0ea59acbee577e7da9
benrybinski <benrybin@gmail.com>
2019-06-24 07:32:19 -0400

    updates

--- src/main/java/com/zipcodewilmington/froilansfarm/MainApplication.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/MainApplication.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 /**
  * Created by leon on 2/26/18.
  */
-public class MainApplication {
+public class
+MainApplication {
     public static void main(String[] args) {
         farmersLife week = new farmersLife();
         week.sunday();
